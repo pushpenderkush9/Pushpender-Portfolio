42a12a0d14aba702c83a95fef314b8f8b2e60d34
w3schools-test <pushpendersharmakush@gmail.com>
2025-02-17 11:10:26 +0530

    Updated portfolio with new changes as second time

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -17,18 +17,11 @@ export default function About() {
               About Me
             </h1>
             <p className="mt-4  text-sm md:text-base leading-relaxed">
-            A MERN stack developer is a highly skilled professional specializing in 
-            building dynamic and scalable web applications using MongoDB, Express.js,
-             React, and Node.js. With expertise in front-end development, they create 
-             responsive and interactive user interfaces using React.js, ensuring
-              seamless user experiences. On the backend, they leverage Node.js and 
-              Express.js to build robust RESTful APIs, manage authentication, and 
-              optimize server-side performance. MongoDB, a NoSQL database, enables 
-              efficient data storage and retrieval, ensuring high-speed and scalable
-               applications. By mastering the entire development lifecycle—from designing
-                the UI to managing server logic and database operations—MERN stack 
-                developers play a crucial role in delivering modern, full-stack web 
-                solutions.
+            I am a MERN stack developer with a strong passion for building dynamic, scalable, and high-performance web applications. With expertise in MongoDB, Express.js, React, and Node.js, I specialize in developing responsive front-end interfaces and robust back-end systems.
+        
+My front-end development skills allow me to craft seamless and interactive user experiences using React.js, ensuring intuitive and engaging applications. On the backend, I leverage Node.js and Express.js to develop efficient RESTful APIs, manage authentication, and optimize server-side performance. Additionally, I utilize MongoDB, a powerful NoSQL database, to ensure fast, scalable, and structured data management.
+
+From designing intuitive UIs to handling server-side logic and database operations, I excel in the full-stack development lifecycle, delivering modern web solutions that meet both technical and business needs.
             </p>
           </div>
         </div>
