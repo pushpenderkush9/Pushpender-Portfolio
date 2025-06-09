862722f952daaa537dc153d37592a1107c5d52e8
w3schools-test <pushpendersharmakush@gmail.com>
2025-06-09 14:52:37 +0530

    6th edit commit

--- src/components/ProjectList.jsx
+++ src/components/ProjectList.jsx
@@ -1,10 +1,28 @@
 import { useNavigate } from "react-router-dom";
 const projects = [
+     {
+        name:"CODE-X-CAMPUS",
+        img:"./projects/code.png",
+        link:"https://code-x-campus.vercel.app/",
+        description:"Created Code-X-Campus, a complete e-learning platform built with Next.js, offering government-approved internships, affordable certification, and real-time learning experiences for students to grow their skills and gain industry exposure.",
+    },
+     {
+        name:"ShopEase",
+        img:"./projects/ShopEase.png",
+        link:"https://github.com/pushpenderkush9/Shop",
+        description:"Developed ShopEase, a full-stack e-commerce site for clothes, shoes, and accessories using React, Vite, Node.js, Express, and MongoDB.",
+    },
     {
         name: "SKill@meet",
         img: "./projects/skill.png",
         link: "https://github.com/pushpenderkush9/Skill-meet",
         description: "A social media platform where people can meet up and chat ."
+    },
+    {
+        name:"Node.js QR Code Generator",
+        img:"./projects/qr.png",
+        link:"https://github.com/pushpenderkush9/node-qr-generater",
+        description:"Built a Node.js QR Code Generator that saves both the QR image and the input URL using Inquirer, QR-Image, and FS.",
     },
     {
         name: "3d flip card",
@@ -25,6 +43,12 @@ const projects = [
         description: "Its a Weather app which can show temparature and humidity of a place based on api data."
     },
     {
+        name:"Profile card",
+        img:"./projects/profileCard.png",
+        link: "https://github.com/pushpenderkush9/profile-card",
+        description:"Responsive profile card with social icons, stats, and action buttons.",
+    },
+    {
         name: "Responsive Page",
         img: "./projects/responsive-page.png",
         link: "https://github.com/pushpenderkush9/OBISIP/tree/main/responsive-landing-page",
@@ -34,9 +58,9 @@ const projects = [
         name: "STS-STT-TTS",
         img: "./projects/voice.png",
         link: "https://github.com/pushpenderkush9/STS-STT-TTS/tree/main/voicetovoice",
-        description: "A react based Voice to  voice converter which can convert voice into voice and voice into text ."
-    }
-
+        description: "A react based Voice to  voice converter which can convert voice into voice and voice into text .",
+    },
+    
 ];
 
 
